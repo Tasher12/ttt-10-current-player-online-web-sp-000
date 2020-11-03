0b61993f8da67d62431df1a1822bf1c021ac243a
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-11-03 16:09:44 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,8 +4,8 @@ board = ["","","","","","","","",""]
 def turn_count(board)
   counter = 0 
   board.each {|index|
-    if index == "x" || index == "o"
-    counter += 1}
+    if index == "x" || index == "o"}
+    counter += 1
   end
   counter 
 end 
