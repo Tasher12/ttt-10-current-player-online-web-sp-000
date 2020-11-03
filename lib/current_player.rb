bbb303eab467517d175cf08b5947b643e5b5959c
authentic-pipe-8414 <tasher.stidham@outlook.com>
2020-11-03 16:08:00 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,23 @@
+#This code keeps track of how many turns have been made
+board = ["","","","","","","","",""]
+
+def turn_count(board)
+  counter = 0 
+  board.each do |index|
+    if index == "x" || index == "o"
+    counter += 1 
+  end
+  counter 
+end 
+  
+
+
+#This code keeps track of whos turn it is
+def current_player(board)
+  if turn_count(board).even?
+    puts 'X'
+  elsif turn_count(board).odd?
+    puts 'O'
+  end 
+  end
+end 
